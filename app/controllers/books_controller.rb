2642570b82e0ce486d09463a60a3a6d984ad4642
Aya1996syuger <ayaka.coco87@gmail.com>
2021-10-26 09:22:20 +0000

    commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,14 @@ class BooksController < ApplicationController
   end
 
   def index
-    @book = Book.all
+    @books = Book.all
+    @book = Book.new
   end
 
   def create
       book = Book.new(book_params)
       book.save
-      redirect_to book_path(book.id)
+      redirect_to book_path(book)
   end
 
   def show
@@ -22,16 +23,22 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path
+    redirect_to books_path
   end
 
     private
   # ストロングパラメータ
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 end
