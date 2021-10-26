021c3b2e1e1365eb9f304255a5813ce9861c01ae
Aya1996syuger <ayaka.coco87@gmail.com>
2021-10-26 09:49:28 +0000

    feat:削除確認

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,12 @@ class BooksController < ApplicationController
   end
 
   def create
-      book = Book.new(book_params)
-      book.save
-      redirect_to book_path(book)
+      @book = Book.new(book_params)
+      if @book.save
+        redirect_to book_path(@book.id)
+     else
+       render:index
+      end
   end
 
   def show
